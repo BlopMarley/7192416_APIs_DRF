f75e187c703fdc606a1818ec713e1d37ae407928
Simon Garreau <s.garreau@geofit.fr>
2022-11-30 16:34:32 +0100

    centralize everything

--- shop/serializer.py
+++ shop/serializer.py
@@ -1,17 +1,31 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, SerializerMethodField
 from shop.models import Article, Category, Product
- 
-class CategorySerializer(ModelSerializer):
-    class Meta:
-        model = Category
-        fields = ['id', 'name']
-
-class ProductSerializer(ModelSerializer):
-    class Meta:
-        model = Product
-        fields = ['id', 'date_created', 'date_updated', 'name', 'category']
 
 class ArticleSerializer(ModelSerializer):
     class Meta:
         model = Article
         fields = ['id', 'date_created', 'date_updated', 'name', 'price', 'product']
+
+class ProductSerializer(ModelSerializer):
+
+    articles = SerializerMethodField()
+    class Meta:
+        model = Product
+        fields = ['id', 'date_created', 'date_updated', 'name', 'category', 'articles']
+
+    def get_articles(self, instance):
+        queryset = instance.articles.filter(active=True)
+        serializer = ArticleSerializer(queryset, many=True)
+        return serializer.data
+
+class CategorySerializer(ModelSerializer):
+
+    products = SerializerMethodField()
+    class Meta:
+        model = Category
+        fields = ['id', 'date_created', 'date_updated', 'name', 'products']
+    
+    def get_products(self, instance):
+        queryset = instance.products.filter(active=True)
+        serializer = ProductSerializer(queryset, many=True)
+        return serializer.data
