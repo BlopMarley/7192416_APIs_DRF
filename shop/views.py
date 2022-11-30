6c85469abe5cf77fba720280aaad89544d9e178e
Simon Garreau <s.garreau@geofit.fr>
2022-11-30 14:57:53 +0100

    endpoint for product api

--- shop/views.py
+++ shop/views.py
@@ -1,11 +1,18 @@
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from shop.models import Category
-from shop.serializer import CategorySerializer
+from shop.models import Category, Product
+from shop.serializer import CategorySerializer, ProductSerializer
 
 class CategoryView(APIView):
 
     def get(self, *args, **kwargs):
         categories = Category.objects.all()
         serializer = CategorySerializer(categories, many=True)
+        return Response(serializer.data)
+
+class ProductView(APIView):
+
+    def get(self, *args, **kwargs):
+        products = Product.objects.all()
+        serializer = ProductSerializer(products, many=True)
         return Response(serializer.data)
